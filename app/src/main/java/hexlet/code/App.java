6adb9c95958f40ec065065170b79420846a56548
Alexey Besedin <cdr800mb@mail.ru>
2023-04-07 22:41:08 +0400

    Add picocli framework to project and make initial cli conf

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class App {
-    public static void main(String[] args) {
+@Command(name = "gendiff",
+         version = "gendiff 1.0",
+         mixinStandardHelpOptions = true,
+         description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+
+    }
+
 }
