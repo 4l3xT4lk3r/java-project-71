fb6a8b9c40c47f52d9532a4202e899393732349d
Alexey Besedin <cdr800mb@mail.ru>
2023-04-29 00:37:37 +0400

    Replace static block with Map.of in FormatterUtil class

--- app/src/main/java/hexlet/code/FormatterUtil.java
+++ app/src/main/java/hexlet/code/FormatterUtil.java
@@ -7,16 +7,14 @@ import hexlet.code.formatters.StylishFormatter;
 import hexlet.code.formatters.PlainFormatter;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 public final class FormatterUtil {
-    private static final HashMap<String, Formatter> FORMATTERS = new HashMap<>();
-
-    static {
-        FORMATTERS.put("json", new JsonFormatter());
-        FORMATTERS.put("stylish", new StylishFormatter());
-        FORMATTERS.put("plain", new PlainFormatter());
-    }
+    private static final Map<String, Formatter> FORMATTERS = Map.of(
+            "json", new JsonFormatter(),
+            "stylish", new StylishFormatter(),
+            "plain", new PlainFormatter());
 
     public static String format(TreeMap<String, HashMap<String, Object>> map,
                                 String formatter) throws JsonProcessingException {
